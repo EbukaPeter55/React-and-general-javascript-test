0a4ac28ce3993547e61c8b71a5f432db33db7256
Ebuka Peter <peterdbrainy5@gmail.com>
2021-10-06 05:46:52 +0100

    first unit test

---
+++ script.js
@@ -0,0 +1,20 @@
+const googleDatabase = [
+    'cats.com',
+    'souprecipes.com',
+    'flowers.com',
+    'animals.com',
+    'catpictures.com',
+    'myfavouritecats.com',
+    'myfavouritecats2.com'
+];
+
+
+const googleSearch = (searchInput) => {
+    const matches = googleDatabase.filter(website => {
+        return website.includes(searchInput);
+    })
+    return matches.length > 3 ? matches.slice(0, 3) : matches;
+}
+
+console.log(googleSearch('soup'));
+// googleSearch(soup);
