539c5cae9836e9a76545e4572a994189be6d8df8
Ebuka Peter <peterdbrainy5@gmail.com>
2021-10-07 03:11:18 +0100

    Add a watch to jest in packagejson to enable reload on each change in js files. Add a test for the google search function using a mock database

--- script.js
+++ script.js
@@ -9,12 +9,15 @@ const googleDatabase = [
 ];
 
 
-const googleSearch = (searchInput) => {
-    const matches = googleDatabase.filter(website => {
+const googleSearch = (searchInput, db) => {
+    const matches = db.filter(website => {
         return website.includes(searchInput);
     })
     return matches.length > 3 ? matches.slice(0, 3) : matches;
 }
 
-console.log(googleSearch('soup'));
+// console.log(googleSearch('soup', googleDatabase));
 // googleSearch(soup);
+
+
+module.exports = googleSearch;
--- script.test.js
+++ script.test.js
@@ -1,3 +1,17 @@
-it('this is a test', ()=>{
+const googleSearch = require('./script');
+
+dbMock = [
+    'dog.com',
+    'cheesepuff.com',
+    'dogpictures.com'
+];
+
+it('Is a silly test', ()=>{
+    expect('hello').toBe('hello')
     
+})
+
+it('is searching google', () => {
+    expect(googleSearch('test', dbMock)).toEqual([])
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
 })
