454c086daaf66091bb3c25ee27a3118d153b983b
Ebuka Peter <peterdbrainy5@gmail.com>
2021-10-08 03:43:37 +0100

    Write a test testing that the function accepts undefined and null. Also another test testing than it does not return more than 3 matches

--- script.test.js
+++ script.test.js
@@ -11,7 +11,20 @@ it('Is a silly test', ()=>{
     
 })
 
+// Testing the search google function
 it('is searching google', () => {
-    expect(googleSearch('test', dbMock)).toEqual([])
-    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
+    expect(googleSearch('test', dbMock)).toEqual([]);
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+})
+
+// Test that the function logic works with undefined and null inputs
+it('work with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+})
+
+// Testing that our search does not return more than 3 matches
+it('It does not return more than 3 matches', () => {
+    expect(googleSearch('.com', dbMock).length).toEqual(3);
+    // expect(googleSearch(null, dbMock)).toEqual([])
 })
