1752420da842bd112e4c5f0d636f93623dca6585
Ebuka Peter <peterdbrainy5@gmail.com>
2021-10-08 03:53:29 +0100

    Group all test into a describe function because they are similar

--- script.test.js
+++ script.test.js
@@ -6,25 +6,30 @@ dbMock = [
     'dogpictures.com'
 ];
 
-it('Is a silly test', ()=>{
-    expect('hello').toBe('hello')
+// Group similar test together using the Describe
+describe ('googleSearch', () => {
+
+    it('Is a silly test', ()=>{
+        expect('hello').toBe('hello')
+        
+    })
     
-})
-
-// Testing the search google function
-it('is searching google', () => {
-    expect(googleSearch('test', dbMock)).toEqual([]);
-    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
-})
-
-// Test that the function logic works with undefined and null inputs
-it('work with undefined and null input', () => {
-    expect(googleSearch(undefined, dbMock)).toEqual([]);
-    expect(googleSearch(null, dbMock)).toEqual([]);
-})
-
-// Testing that our search does not return more than 3 matches
-it('It does not return more than 3 matches', () => {
-    expect(googleSearch('.com', dbMock).length).toEqual(3);
-    // expect(googleSearch(null, dbMock)).toEqual([])
+    // Testing the search google function
+    it('is searching google', () => {
+        expect(googleSearch('test', dbMock)).toEqual([]);
+        expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+    })
+    
+    // Test that the function logic works with undefined and null inputs
+    it('work with undefined and null input', () => {
+        expect(googleSearch(undefined, dbMock)).toEqual([]);
+        expect(googleSearch(null, dbMock)).toEqual([]);
+    })
+    
+    // Testing that our search does not return more than 3 matches
+    it('It does not return more than 3 matches', () => {
+        expect(googleSearch('.com', dbMock).length).toEqual(3);
+        // expect(googleSearch(null, dbMock)).toEqual([])
+    })
+    
 })
